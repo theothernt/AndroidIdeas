446b7d92fb3adecdc9c98e7637ae9f1b05519c4a
Neil Turner <neilturner@gmail.com>
2026-01-19 23:55:50 +0000

    Sample text tweaks

--- OverlayParty/app/src/main/java/com/neilturner/overlayparty/data/LocationRepository.kt
+++ OverlayParty/app/src/main/java/com/neilturner/overlayparty/data/LocationRepository.kt
@@ -6,16 +6,16 @@ import kotlinx.coroutines.flow.flow
 
 class LocationRepository {
     private val scenicLocations = listOf(
-        "Grand Canyon, USA - A immense steep-sided canyon carved by the Colorado River in Arizona, known for its visually overwhelming size and colorful landscape.",
-        "Great Barrier Reef, Australia - The world's largest coral reef system composed of over 2,900 individual reefs.",
-        "Machu Picchu, Peru - A 15th-century Inca citadel situated on a mountain ridge 2,430 metres above sea level, often referred to as the 'Lost City of the Incas'.",
+        "Grand Canyon, USA - An immense steep-sided canyon carved by the Colorado River.",
+        "Great Barrier Reef, Australia - The world's largest coral reef system.",
+        "Machu Picchu, Peru - A 15th-century Inca citadel situated on a mountain ridge.",
         "Aurora Borealis, Iceland - A natural light display in the Earth's sky.",
         "Victoria Falls, Zambia/Zimbabwe - A waterfall on the Zambezi River in southern Africa, providing habitat for several unique species of plants and animals.",
         "Mount Everest, Nepal - The Earth's highest mountain above sea level.",
-        "Petra, Jordan - A historical and archaeological city in southern Jordan, famous for its rock-cut architecture and water conduit system, established around 312 BC.",
+        "Petra, Jordan - A historical and archaeological city in southern Jordan.",
         "Taj Mahal, India - An ivory-white marble mausoleum on the southern bank of the river Yamuna in the Indian city of Agra, commissioned in 1632.",
         "Santorini, Greece - An island in the southern Aegean Sea.",
-        "Banff National Park, Canada - Canada's oldest national park, established in 1885, known for its rocky mountain peaks, turquoise glacial lakes, and abundant wildlife."
+        "Banff National Park, Canada - Canada's oldest national park, established in 1885."
     )
 
     fun getLocationStream(): Flow<String> = flow {
