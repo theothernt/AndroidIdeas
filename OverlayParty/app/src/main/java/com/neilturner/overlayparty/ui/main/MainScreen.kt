6dfd098fa0b388ab2a5b02691882e256f0a04d9f
Neil Turner <neilturner@gmail.com>
2026-01-20 14:20:13 +0000

    Added basic gradient

--- OverlayParty/app/src/main/java/com/neilturner/overlayparty/ui/main/MainScreen.kt
+++ OverlayParty/app/src/main/java/com/neilturner/overlayparty/ui/main/MainScreen.kt
@@ -5,6 +5,8 @@ import androidx.compose.animation.core.tween
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
@@ -14,7 +16,9 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 import com.neilturner.overlayparty.ui.overlay.OverlayLayout
 import kotlinx.coroutines.delay
 import org.koin.androidx.compose.koinViewModel
@@ -44,6 +48,23 @@ fun MainScreen(viewModel: MainViewModel = koinViewModel()) {
             .background(Color.Black)
     ) {
         Slideshow()
+
+        // Gradient at the top to make overlays more readable
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(300.dp)
+                .background(
+                    Brush.verticalGradient(
+                        0.00f to Color.Black.copy(alpha = 0.80f),
+                        0.25f to Color.Black.copy(alpha = 0.55f),
+                        0.50f to Color.Black.copy(alpha = 0.30f),
+                        0.70f to Color.Black.copy(alpha = 0.15f),
+                        0.85f to Color.Black.copy(alpha = 0.05f),
+                        1.00f to Color.Transparent
+                    )
+                )
+        )
 
         OverlayLayout(
             topStart = topStart,
