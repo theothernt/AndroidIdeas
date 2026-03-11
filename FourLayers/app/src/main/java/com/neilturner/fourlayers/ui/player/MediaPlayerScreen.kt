b53835f360603e095623be7805e42cb06279dcda
Neil Turner <neilturner@gmail.com>
2026-03-11 19:45:54 +0000

    Fixed view state restoration

--- FourLayers/app/src/main/java/com/neilturner/fourlayers/ui/player/MediaPlayerScreen.kt
+++ FourLayers/app/src/main/java/com/neilturner/fourlayers/ui/player/MediaPlayerScreen.kt
@@ -3,6 +3,8 @@ package com.neilturner.fourlayers.ui.player
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import com.neilturner.fourlayers.data.MediaProvider
 import org.koin.androidx.compose.koinViewModel
@@ -10,10 +12,14 @@ import org.koin.androidx.compose.koinViewModel
 @Composable
 fun MediaPlayerScreen() {
     val viewModel: MediaPlayerViewModel = koinViewModel()
+    val state by viewModel.state.collectAsState()
 
     LaunchedEffect(Unit) {
-        val playlist = MediaProvider.getSamplePlaylist()
-        viewModel.loadPlaylist(playlist)
+        // Only load playlist if not already loaded
+        if (state.playlist.isEmpty()) {
+            val playlist = MediaProvider.getSamplePlaylist()
+            viewModel.loadPlaylist(playlist)
+        }
     }
 
     MediaPlayer(
