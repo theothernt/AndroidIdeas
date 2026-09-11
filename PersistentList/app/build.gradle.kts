a5a21eb0ff7be525b5245e6db0f0476fa8a49f08
Neil Turner <neil@neilturner.com>
2026-09-11 21:21:14 +0100

    Updated libs and build settings

--- PersistentList/app/build.gradle.kts
+++ PersistentList/app/build.gradle.kts
@@ -23,8 +23,8 @@ android {
 
 	defaultConfig {
 		applicationId = "com.neilturner.persistentlist"
-		minSdk = 24
-		targetSdk = 36
+		minSdk = 26
+		targetSdk = 37
 		versionCode = 1
 		versionName = "1.0"
 
