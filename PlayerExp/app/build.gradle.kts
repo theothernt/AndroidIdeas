98d32185d080c5c718b91cbb12b92137436961ff
Neil Turner <neil@neilturner.com>
2026-09-22 17:54:39 +0100

    Add media3 compose

--- PlayerExp/app/build.gradle.kts
+++ PlayerExp/app/build.gradle.kts
@@ -53,6 +53,7 @@ dependencies {
 	implementation(libs.androidx.media3.exoplayer.hls)
 	implementation(libs.androidx.media3.ui)
 	implementation(libs.androidx.media3.datasource.okhttp)
+	implementation(libs.androidx.media3.ui.compose)
 	androidTestImplementation(libs.androidx.compose.ui.test.junit4)
 	debugImplementation(libs.androidx.compose.ui.test.manifest)
 	debugImplementation(libs.androidx.compose.ui.tooling)
